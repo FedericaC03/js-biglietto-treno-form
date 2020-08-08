45370c7d20bb347ed5195fed0dd593605dd1706d
FedericaC03 <fdrk03@hotmail.it>
2020-08-08 15:57:05 +0200

    Completato javascript per la sezione descrizione biglietto

--- js/script.js
+++ js/script.js
@@ -9,5 +9,34 @@ function() {
   var km = document.getElementById("km").value;
   var eta = document.getElementById("eta").value;
   document.getElementById('nome-passeggero').innerHTML = nome;
+
+//DEFINISCO L'OFFERTA IN BASE ALL'ETA' DELL'UTENTE
+  var prezzobiglietto = document.getElementById("prezzo-biglietto");
+  var prezzoxkm = km * 0.21;
+  var sconto = document.getElementById('sconto');
+  if (eta == "minorenne") {
+    sconto.innerHTML = "sconto minorenni";
+  } else if (eta == "over65") {
+    sconto.innerHTML = "prezzo silver";
+  } else {
+    sconto.innerHTML = "prezzo standard";
+  }
+  //GENERO UN NUMERO CASUALE DELLA CARROZZA
+  var carrozza = document.getElementById("carrozza");
+  carrozza.innerHTML = Math.floor(Math.random() * 9) + 1;
+
+  //GENERO UN NUMERO CASUALE DEL CAMBIO PRENOTAZIONE
+  var prenotazione = document.getElementById("prenotazione");
+  prenotazione.innerHTML = Math.floor(Math.random() * 9999) + 90000;
+
+  //CALCOLO IL PREZZO DEL BIGLIETTO IN BASE AI KM E ALL'ETA
+  var prezzobiglietto = document.getElementById("prezzoBiglietto");
+  if ( eta == "minorenne") {
+    prezzobiglietto.innerHTML = ((prezzoxkm - ((prezzoxkm * 20) / 100))).toFixed(2);
+  } else if ( eta == "over65") {
+    prezzobiglietto.innerHTML = ((prezzoxkm - ((prezzoxkm * 40) / 100))).toFixed(2);
+  } else {
+    prezzobiglietto.innerHTML = prezzoxkm;
+  }
 }
 );
